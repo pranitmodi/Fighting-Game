2fc62c2f044269e488230b09ab05f9f28f914ef3
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-08-04 19:14:13 +0530

    DONE

--- script2.js
+++ script2.js
@@ -7,8 +7,35 @@ const d2 = document.getElementById('na2')
 const score1 = document.getElementById('v1')
 const score2 = document.getElementById('v2')
 
+const res = document.getElementById('res')
+
+let c = 1
+
 let s1
 let s2
+
+const display = () =>
+{
+    score1.innerText = `Health: ${Math.trunc(s1)}`
+    score2.innerText = `Health: ${Math.trunc(s2)}`
+
+    if(s1 < 0)
+    {
+        res.innerText = `${n2} Won`
+        document.getElementById('vic').play()
+        c = 0
+    }
+    else if(s2 < 0)
+    {
+        res.innerText = `${n1} Won`
+        document.getElementById('vic').play()
+        c = 0
+    }
+    else
+    {
+        res.innerText = ''
+    }
+}
 
 const process = () =>
 {
@@ -18,18 +45,43 @@ const process = () =>
     s1 = 100
     s2 = 100
 
-    score1.innerText = `Score: ${s1}`
-    score2.innerText = `Score: ${s2}`
+    display()
 }
-process()
 
-const a = ducument.getElementById('fheal')
-console.log(a)
+process()
 
 document.addEventListener('keydown',function(e)
 {
-    if(e.key == 'Q')
+    if(c == 1)
     {
-        ducument.getElementById('fpunch').play()
+        if(e.key == 'Q' || e.key == 'q')
+        {
+            s2 = s2 - (Math.random()*10)
+            document.getElementById('fpunch').play()
+        }
+        else if(e.key == 'A' || e.key == 'a')
+        {
+            s1 = s1 + (Math.random()*10)
+            document.getElementById('fheal').play()
+        }
+        else if(e.key == 'P' || e.key == 'p')
+        {
+            s1 = s1 - (Math.random()*10)
+            document.getElementById('qhit').play()
+        }
+        else if(e.key == 'L' || e.key == 'l')
+        {
+            s2 = s2 + (Math.random()*10)
+            document.getElementById('qheal').play()
+        }
+        display()
     }
 })
+
+function reset()
+{
+    c = 1
+    s1 = 100
+    s2 = 100
+    display()
+}
